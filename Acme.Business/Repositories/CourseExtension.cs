72a13c6f55a2942445a9445921282ba5dc192fcb
Fernando Vilhena <engesoft@hotmail.com>
2018-08-01 10:36:56 -0300

    Added Repository as extension of DbSet.

---
+++ Acme.Business/Repositories/CourseExtension.cs
@@ -0,0 +1,22 @@
+﻿using Acme.Business.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Acme.Business.Repositories
+{
+    public static class CourseExtension
+    {
+        public static List<Course> GetAll(this DbSet<Course> db)
+        {
+            return db.ToList();
+        }
+
+        public static Course GetById(this DbSet<Course> db, int id)
+        {
+            return db.FirstOrDefault(c => c.Id == id);
+        }
+    }
+}
