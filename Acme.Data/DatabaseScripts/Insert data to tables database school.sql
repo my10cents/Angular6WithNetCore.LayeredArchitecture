d46e5892d75a1bd2a75ec55e8da27442182ad5f9
Fernando Vilhena <engesoft@hotmail.com>
2018-10-03 17:36:20 -0300

    Added scripts to create tables and add example data

---
+++ Acme.Data/DatabaseScripts/Insert data to tables database school.sql
@@ -0,0 +1,31 @@
+﻿insert into School..Course values (1,'Chemistry',3);
+insert into School..Course values (2,'Microeconomics',3);
+insert into School..Course values (3,'Macroeconomics',3);
+insert into School..Course values (4,'Calculus',4);
+insert into School..Course values (5,'Trigonometry',4);
+insert into School..Course values (6,'Composition',3);
+insert into School..Course values (7,'Literature',4);
+GO
+
+insert into School..Student values (1,'Alexander','Carson','2010-09-01 00:00:00');
+insert into School..Student values (2,'Alonso','Meredith','2012-09-01 00:00:00');
+insert into School..Student values (3,'Anand','Arturo','2013-09-01 00:00:00');
+insert into School..Student values (4,'Barzdukas','Gytis','2012-09-01 00:00:00');
+insert into School..Student values (5,'Li','Yan','2012-09-01 00:00:00');
+insert into School..Student values (6,'Justice','Peggy','2011-09-01 00:00:00');
+insert into School..Student values (7,'Norman','Laura','2013-09-01 00:00:00');
+insert into School..Student values (8,'Olivetto','Nino','2005-08-11 00:00:00');
+GO
+
+insert into School..Enrollment values (1,1,1,'A');
+insert into School..Enrollment values (2,2,1,'C');
+insert into School..Enrollment values (3,3,1,'B');
+insert into School..Enrollment values (4,4,2,'B');
+insert into School..Enrollment values (5,5,2,'B');
+insert into School..Enrollment values (6,6,2,'B');
+insert into School..Enrollment values (7,1,3,'C');
+insert into School..Enrollment values (8,2,3,'B');
+insert into School..Enrollment values (9,1,4,'B');
+insert into School..Enrollment values (10,6,5,'B');
+insert into School..Enrollment values (11,7,6,'B');
+go
