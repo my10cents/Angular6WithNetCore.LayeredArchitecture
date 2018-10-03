0f5fa4e021ef359f8f13b2ac3697527056d5125b
Fernando Vilhena <engesoft@hotmail.com>
2018-10-03 17:35:17 -0300

    Added IDbContext to use dependency injection

---
+++ Acme.Business/Data/IDbContext.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Acme.Business.Data
+{
+    public interface IDbContext
+    {
+        IModel Model { get; }
+        ChangeTracker ChangeTracker { get; }
+
+        EntityEntry Add(object entity);
+        EntityEntry<TEntity> Add<TEntity>(TEntity entity) where TEntity : class;
+        Task<EntityEntry<TEntity>> AddAsync<TEntity>(TEntity entity, CancellationToken cancellationToken = default(CancellationToken)) where TEntity : class;
+        Task<EntityEntry> AddAsync(object entity, CancellationToken cancellationToken = default(CancellationToken));
+        void AddRange(IEnumerable<object> entities);
+        void AddRange(params object[] entities);
+        Task AddRangeAsync(IEnumerable<object> entities, CancellationToken cancellationToken = default(CancellationToken));
+        Task AddRangeAsync(params object[] entities);
+        EntityEntry<TEntity> Attach<TEntity>(TEntity entity) where TEntity : class;
+        EntityEntry Attach(object entity);
+        void AttachRange(params object[] entities);
+        void AttachRange(IEnumerable<object> entities);
+        void Dispose();
+        EntityEntry<TEntity> Entry<TEntity>(TEntity entity) where TEntity : class;
+        EntityEntry Entry(object entity);
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        bool Equals(object obj);
+        object Find(Type entityType, params object[] keyValues);
+        TEntity Find<TEntity>(params object[] keyValues) where TEntity : class;
+        Task<object> FindAsync(Type entityType, params object[] keyValues);
+        Task<TEntity> FindAsync<TEntity>(object[] keyValues, CancellationToken cancellationToken) where TEntity : class;
+        Task<object> FindAsync(Type entityType, object[] keyValues, CancellationToken cancellationToken);
+        Task<TEntity> FindAsync<TEntity>(params object[] keyValues) where TEntity : class;
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        int GetHashCode();
+        EntityEntry Remove(object entity);
+        EntityEntry<TEntity> Remove<TEntity>(TEntity entity) where TEntity : class;
+        void RemoveRange(IEnumerable<object> entities);
+        void RemoveRange(params object[] entities);
+        int SaveChanges(bool acceptAllChangesOnSuccess);
+        int SaveChanges();
+        Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default(CancellationToken));
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default(CancellationToken));
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        string ToString();
+        EntityEntry Update(object entity);
+        EntityEntry<TEntity> Update<TEntity>(TEntity entity) where TEntity : class;
+        void UpdateRange(params object[] entities);
+        void UpdateRange(IEnumerable<object> entities);
+    }
+}
