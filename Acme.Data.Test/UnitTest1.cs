99c9c0c62b640cb4cd6d458819321d19f48f4528
Fernando Vilhena <engesoft@hotmail.com>
2018-10-05 11:04:03 -0300

    added skeleton test project

---
+++ Acme.Data.Test/UnitTest1.cs
@@ -0,0 +1,26 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Acme.Data.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(true, true);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.AreEqual(true, true);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Assert.AreEqual(true, true);
+        }
+    }
+}
